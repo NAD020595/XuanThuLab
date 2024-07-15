8654f55c7f8776572cb7c2d9c5ad87310139c67f
Duy Nguyen <nguyenanhduy02051995@gmail.com>
2024-07-15 22:13:43 +0700

    Change Directory

--- CSharpDotNet/Part1_Introductory/CS001_HelloWorld/obj/Debug/net8.0/CS001_HelloWorld.AssemblyInfo.cs
+++ CSharpDotNet/Part1_Introductory/CS001_HelloWorld/obj/Debug/net8.0/CS001_HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CS001_HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+253c976f05fc47379e3684c7ddd9a8cdc0476f4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CS001_HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CS001_HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
